65d69489b4f632b3b1be1798dfa8bf0e08b8a946
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-09-25 16:42:29 +0300

    Правка пома

--- src/main/java/ru/study/crud/dto/StudentDto.java
+++ src/main/java/ru/study/crud/dto/StudentDto.java
@@ -4,45 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/*@Data
+@Data
 @NoArgsConstructor
-@AllArgsConstructor*/
+@AllArgsConstructor
 public class StudentDto {
 
     private int id;
     private String name;
     private String surname;
 
-    public StudentDto(int id, String name, String surname) {
-        this.id = id;
-        this.name = name;
-        this.surname = surname;
-    }
 
-    public StudentDto() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
 }
