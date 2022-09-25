38c879ab4961623e1ddbaac5f6e636785c831ab8
Yatsukin <79377539+Yatsukin@users.noreply.github.com>
2022-09-25 18:04:18 +0300

    Мелкие правки в классе

--- src/main/java/ru/study/crud/service/impl/UserServiceImpl.java
+++ src/main/java/ru/study/crud/service/impl/UserServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class UserServiceImpl  {
+public class UserServiceImpl implements UserService {
 
     Map<Integer, StudentDto> studentDtoMap;
 
@@ -15,23 +15,23 @@ public class UserServiceImpl  {
         this.studentDtoMap = studentMap;
     }
 
-
+    @Override
     public List<StudentDto> findAllStudent() {
-        List<StudentDto> students = new ArrayList<StudentDto>(studentDtoMap.values());
+        List<StudentDto> students = new ArrayList<>(studentDtoMap.values());
         return students;
     }
 
-
+    @Override
     public StudentDto findStudentById(int id) {
         return studentDtoMap.get(id);
     }
 
-
+    @Override
     public void save(StudentDto studentDto) {
         studentDtoMap.put(studentDto.getId(), studentDto);
     }
 
-
+    @Override
     public void delete(int id) {
         studentDtoMap.remove(id);
     }
